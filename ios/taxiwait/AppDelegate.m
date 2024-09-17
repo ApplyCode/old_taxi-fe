fc79b9e0dc4db6d108cfb5651e8a2dbff531842a
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-17 01:17:49 -0400

    Update AppDelegate.m

--- ios/taxiwait/AppDelegate.m
+++ ios/taxiwait/AppDelegate.m
@@ -1,4 +1,8 @@
+#import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
