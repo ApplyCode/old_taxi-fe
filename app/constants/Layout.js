33b54fb72b38902e546d487eb52603db6834f366
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-22 10:48:52 -0400

    Update Layout.js

--- app/constants/Layout.js
+++ app/constants/Layout.js
@@ -1,7 +1,6 @@
 import { Dimensions } from 'react-native';
 
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
+
 const Country_City = [
   { label: "Albania- Tirana", value: "2" },
   { label: "Andorra- Andorra la Vella ", value: "3" },
