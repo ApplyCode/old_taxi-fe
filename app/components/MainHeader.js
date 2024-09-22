9411c51dc6bcf27ffa1ed51144ff77d87ff8c435
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-22 10:46:33 -0400

    Update MainHeader.js

--- app/components/MainHeader.js
+++ app/components/MainHeader.js
@@ -39,4 +39,13 @@ export default class MainHeader extends Component {
   }
 }
 
-
+const styles = StyleSheet.create({
+    header: {
+        height: 50,
+        backgroundColor: '#ffc500',
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingLeft: 20,
+    }
+})
