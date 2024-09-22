e8c86a4016ac93763e363a274b3a62b1d0607c8b
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-22 10:48:31 -0400

    Update Global.js

--- app/constants/Global.js
+++ app/constants/Global.js
@@ -1,11 +1,2 @@
-import {Toast} from 'native-base';
-export async function showToast(text = '', type = 'danger', position = 'bottom') {
-    Toast.show({
-        text: text == '' ? 'Network Error!' : text,
-        type: type,
-        textStyle: {textAlign: 'center'},
-        position: position,
-        duration: 4000,
-    }); 
-}
+
 
