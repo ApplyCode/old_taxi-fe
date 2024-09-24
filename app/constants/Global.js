65e19b26521d49bf59b44c09f4571cea86d6f856
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-24 18:24:34 +0100

    Update Global.js

--- app/constants/Global.js
+++ app/constants/Global.js
@@ -4,6 +4,7 @@ export async function showToast(text = '', type = 'danger', position = 'bottom')
         text: text == '' ? 'Network Error!' : text,
         type: type,
         textStyle: {textAlign: 'center'},
-
+        position: position,
+        duration: 4000,
     }); 
 }
