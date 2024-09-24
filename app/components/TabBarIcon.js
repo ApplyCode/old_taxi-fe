bf35551b488ff28ccd839426f24f01758a5198e8
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-24 18:23:41 +0100

    Update TabBarIcon.js

--- app/components/TabBarIcon.js
+++ app/components/TabBarIcon.js
@@ -1,4 +1,4 @@
-import { Icon } from 'react-native-elements';
+
 import * as React from 'react';
 import { StyleSheet , View , Text, Platform, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
