05b57d1a4fd15bfaa3cf05536328f624e1818112
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-25 05:36:11 -0400

    Update Api.js

--- app/constants/Api.js
+++ app/constants/Api.js
@@ -34,6 +34,7 @@ function createCall(path, data = null, headers = {}, method = 'POST') {
 }
 
 export function client_signup(user, password, email, phone, country, type,phone_code, kind, car_color, plate_no, face, front, back){
+    let backImg = back && back.base64 ? back.base64 : null
 
     return createCall(
         'client_signup',
