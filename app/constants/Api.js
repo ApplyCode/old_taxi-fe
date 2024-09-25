e089ccd2f92180a6abcf93809f4ad4c066449535
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-25 05:35:39 -0400

    Update Api.js

--- app/constants/Api.js
+++ app/constants/Api.js
@@ -16,7 +16,14 @@ function createCall(path, data = null, headers = {}, method = 'POST') {
     }
 
     let strData = JSON.stringify(body);
-
+    if(method == 'POST')
+        return fetch(
+            `${base_url}${path}`, {
+                method : method,
+                headers: merged,
+                body: strData,
+            },
+        ).then((resp) => resp.json());
     else if(method == 'GET')
         return fetch(
             `${base_url}${path}`, {
